621ca5f73137452b6d0eb9fc748921f875e5f17b
jjortegabrito <jjortegabrito@gmail.com>
2022-02-04 23:17:22 -0500

    Added flix assignment to repository

---
+++ Flix Assignment/MoviesCell.swift
@@ -0,0 +1,27 @@
+//
+//  MoviesCell.swift
+//  Flix Assignment
+//
+//  Created by Justin Ortega Brito on 2/4/22.
+//
+
+import UIKit
+
+class MoviesCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
